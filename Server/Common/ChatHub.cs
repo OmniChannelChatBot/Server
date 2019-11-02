5a9d151401d5151eea012d47a4906e879f683877
Дмитрий Николаевич Вознесенский <kantuzzz@live.com>
2019-11-02 22:52:49 +0300

    ChatHub added

---
+++ Server/Common/ChatHub.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Common
+{
+    /// <summary>
+    /// OmniChannel ChatHub.
+    /// </summary>
+    //[Authorize]
+#pragma warning disable CA1034 // Nested types should not be visible
+    public class ChatHub : Hub
+#pragma warning restore CA1034 // Nested types should not be visible
+    {
+        public ChatHub()
+        {
+        }
+
+        /// <summary>
+        /// Отправка сообщения.
+        /// </summary>
+        /// <param name="message">сообщение.</param>
+        /// <param name="userName">кому сообщение.</param>
+        /// <returns>Task.</returns>
+        //public async Task Send(string message, string userName)
+        //{
+        //    await Clients.All.SendAsync("Send", message, userName);
+        //}
+
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("Receive", message, Context.ConnectionId);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var context = this.Context.GetHttpContext();
+            // получаем кук name
+            if (context.Request.Cookies.ContainsKey("name"))
+            {
+                string userName;
+                if (context.Request.Cookies.TryGetValue("name", out userName))
+                {
+                    //Debug.WriteLine($"name = {userName}");
+                }
+            }
+            //// получаем юзер-агент
+            //Debug.WriteLine($"UserAgent = {context.Request.Headers["User-Agent"]}");
+            //// получаем ip
+            //Debug.WriteLine($"RemoteIpAddress = {context.Connection.RemoteIpAddress.ToString()}");
+
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} вошел в чат");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} покинул в чат");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Server.Common;
 using Server.Settings;
 
 namespace Server
@@ -38,6 +40,24 @@ namespace Server
                 options.Url
                     = Configuration.GetSection("DBApiSettings:Url").Value;
             });
+
+            services.Configure<OAuthSettings>(options =>
+            {
+                options.Url
+                    = Configuration.GetSection("OAuthSettings:Url").Value;
+            });
+
+            // use SignalR
+            services.AddSignalR().AddHubOptions<ChatHub>(options =>
+            {
+                // расширенные ошибки только для хаба
+                options.EnableDetailedErrors = true;
+
+                //TODO: вынести в настройки
+                options.KeepAliveInterval = System.TimeSpan.FromMinutes(1);
+            });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +73,16 @@ namespace Server
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseEndpoints(routes =>
+            {
+                routes.MapHub<ChatHub>("/chat",
+                    options => {
+                        options.Transports = HttpTransportType.WebSockets;
+                    });
+            });
+
+            app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
